21074e6bcb52b20e8119127db9807604468a1bc2
Emanuel-Oliveira <emanuel.oliveira.contato@gmail.com>
2023-06-18 10:58:45 -0300

    :sparkles: feat: Listar todos os usuários

--- ignite-template-introducao-ao-SOLID/src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ ignite-template-introducao-ao-SOLID/src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,19 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const requestedUser = this.usersRepository.findById(user_id);
+
+    if (!requestedUser) {
+      throw new Error("User not found");
+    }
+
+    if (!requestedUser.admin) {
+      throw new Error("User is not admin");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
