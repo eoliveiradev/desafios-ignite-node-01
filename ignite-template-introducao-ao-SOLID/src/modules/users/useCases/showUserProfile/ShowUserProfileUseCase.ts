5eef28fbc9d1685193d56e1ac68f96a4f98fdf3e
Emanuel-Oliveira <emanuel.oliveira.contato@gmail.com>
2023-06-18 10:59:36 -0300

    :sparkles: feat: Ver perfil do usuário

--- ignite-template-introducao-ao-SOLID/src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
+++ ignite-template-introducao-ao-SOLID/src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
@@ -9,7 +9,13 @@ class ShowUserProfileUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const requestedUser = this.usersRepository.findById(user_id);
+
+    if (!requestedUser) {
+      throw new Error("User not found");
+    }
+
+    return requestedUser;
   }
 }
 
