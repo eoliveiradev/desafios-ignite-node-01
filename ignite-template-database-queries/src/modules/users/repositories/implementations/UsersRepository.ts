9d31658d94d6511bbfc8d1861b71ba1eeb5e4a59
Emanuel-Oliveira <emanuel.oliveira.contato@gmail.com>
2023-07-02 20:05:29 -0300

    :sparkles: feat: Methods do UsersRepository

--- ignite-template-database-queries/src/modules/users/repositories/implementations/UsersRepository.ts
+++ ignite-template-database-queries/src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,28 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    return this.repository.
+      createQueryBuilder("users")
+      .leftJoinAndSelect("users.games", "games")
+      .where("users.id = :id", { id: user_id })
+      .getOneOrFail();
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.
+      createQueryBuilder("users")
+      .orderBy("users.first_name", "ASC")
+      .getMany();
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.
+      createQueryBuilder("users")
+      .where("users.first_name = :first_name", { first_name })
+      .andWhere("users.last_name = :last_name", { last_name })
+      .getMany();
   }
 }
